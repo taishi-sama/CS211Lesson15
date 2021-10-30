fd6a49418eb44c010629bafd5f4c87a631ff7788
Alexander Mikhaylov <alexsandr.2002.m@gmail.com>
2021-10-30 10:19:48 +0300

    Add realization of LaterInYear function

--- datatime.cpp
+++ datatime.cpp
@@ -3,3 +3,11 @@ bool is_leap(int year)
 	assert(year >= 0);
 	return (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
 }
+//Возвращает true, если первый день ближе к новому году, иначе возвращает false
+bool LaterInYear(int day1, int month1, int day2, int month2)
+{
+	if (month1 = month2)
+		return (day1 > day2);
+	else
+		return month1 > month2;
+}
